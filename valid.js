d8686aac5f269d8afd7f584b00562dcc9f5ed99c
nikunjkr <nikunjkr078@gmail.com>
2021-02-04 21:53:24 +0530

    Added coverall and fixed valid.js

--- valid.js
+++ valid.js
@@ -3,23 +3,23 @@ const luhn= require('./Luhn_algorithm');
 const assert = require('assert');
 
 
-function test() {
+function unit_test() {
     
     var tests =[
         ["0000000000000000", true],
 		["4111111111111111", true],
 		["5500000000000004", true],
 		["6250941006528599", true],
-		// ["5425233430109903", true],
-		// ["6034883265619896", true],
-		// ["4242424242424249", false]
+		["5425233430109903", true],
+		["6034883265619896", true],
+		["4242424242424249", false]
     ];
     
     for(let i=0; i<tests.length; i++) {
         
-        console.log(luhn(tests[i][0]))
-        assert(luhn(tests[i][0])==true)
+        
+        assert(luhn(tests[i][0])==tests[i][1])
     }
 }
 
-test();
+unit_test();
